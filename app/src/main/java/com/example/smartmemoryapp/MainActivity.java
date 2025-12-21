d1e2032249af74920d1a19c7ab227331c7ea0122
promoody1997-pixel <promoody1997@gmail.com>
2025-12-21 11:00:03 +0200

    Update MainActivity.java

--- app/src/main/java/com/example/smartmemoryapp/MainActivity.java
+++ app/src/main/java/com/example/smartmemoryapp/MainActivity.java
@@ -2,21 +2,12 @@ package com.example.smartmemoryapp;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.widget.TextView;
-import android.view.Gravity;
 
 public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        // إنشاء نص ترحيبي بسيط
-        TextView label = new TextView(this);
-        label.setText("مبروك! \n التطبيق يعمل بنجاح 🚀");
-        label.setTextSize(30);
-        label.setGravity(Gravity.CENTER);
-        
-        // عرض النص على الشاشة
-        setContentView(label);
+        // هذا السطر هو الذي يربط الكود بملف التصميم الذي أنشأناه
+        setContentView(R.layout.activity_main);
     }
 }
